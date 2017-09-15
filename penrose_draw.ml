38af8db94a755c220d22ef6af7422cf969c1f4c9
Poroing <jean-18-09@hotmail.fr>
2017-09-15 17:54:09 +0200

    Dividing

--- penrose_draw.ml
+++ penrose_draw.ml
@@ -12,8 +12,51 @@ let draw points color=
 	set_color color;
 	fill_poly pointsInt;;
 
-draw [|(10.,40.);(200.,100.);(1000.,452.)|] green;;
-Unix.sleep 4;;
+(* The vertex which have an angle different from the two other vertex of
+the triangle is put in the first place of the triangle*)
 
-let x = 2;;
-print_int x;;
+type triangle_type = Obtuse | Acute;;
+
+let phi = (1. +. sqrt 5) /. 2.;;
+
+(* Return the points that is at 1 / (1 + phi) ratio from the first point *)
+let getDividingPoint (x1, y1) (x2, y2) =
+    let x = x1 + (x1 - x2) * 1 /. (1 +. phi) in
+    let y = y1 + (y1 - y2) * 1 /. (1 +. phi) in
+    (x, y)
+;;
+
+let getTriangleColor triangle_type =
+    match triangle_type with
+        | Obtuse -> green
+        | Acute -> red
+;;
+
+let rec divide generations points triangle_type =
+    if generations <= 0 then
+        draw points (getTriangleColor triangle_type)
+    else
+        match triangle_type with
+            | Obtuse -> divideObtuse generations points
+            | Acute -> divideAcute generations points
+
+and divideObtuse generations points =
+    let new_point = getDividingPoint points.(1) points.(2) in
+    let acute_triangle = [| points.(2); new_point; points.(0) |] in
+    let obtuse_triangle = [| new_point; points.(0); points.(1) |] in
+    begin
+        divide (generations - 1) acute_triangle Acute;
+        divide (generations - 1) obtuse_triangle Obtuse
+    end
+
+and divideAcute generations points =
+    (* Instead of cutting into three triangles, we cut into two triangles
+        but in the end it will amount to the same thing *)
+    let new_point1 = getDividingPoint points.(1) points.(0) in
+    let obtuse_triangle = [| new_point; points.(0); points.(2) |] in
+    let acute_triangle = [| points.(2); new_point; points.(1) |] in
+    begin
+        divide (generations - 1) acute_triangle Acute;
+        divide generations obtuse_triangle Obtuse
+    end
+;;
