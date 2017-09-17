760f15921bf207f29e8f8c905e205977e659d53d
Poroing <jean-18-09@hotmail.fr>
2017-09-17 11:20:40 +0200

    A little bit of documentation

--- penrose_draw.ml
+++ penrose_draw.ml
@@ -5,6 +5,7 @@ let floatDoublet double = match double with
 	| (a,b) -> (int_of_float a, int_of_float b);;
 
 let drawLine point1 point2 =
+    (* Draw a line between point1 and point2 *)
     let x1, y1 = floatDoublet point1 in
     let x2, y2 = floatDoublet point2 in
     Graphics.moveto x1 y1;
@@ -12,6 +13,8 @@ let drawLine point1 point2 =
 ;;
 
 let drawHollowPolygon points color = 
+    (* Draw in the given color the edges of the polygons represented by
+        points *)
     let points_length = Array.length points in
     Graphics.set_color color;
     for i=0 to points_length - 1 do
@@ -20,6 +23,7 @@ let drawHollowPolygon points color =
 ;;
 
 let drawFilledPolygon points color = 
+    (* Draw in the given color the polygons represented by points *)
 	let pointsInt = Array.map floatDoublet points in
 	Graphics.set_color color;
 	Graphics.fill_poly pointsInt
@@ -88,10 +92,12 @@ and divideAcute generations points =
 ;;
 
 let getAcuteTriangle size position =
+    (* size is the size of the side with unit length *)
     let height = sqrt (phi *. phi -. 0.25) in
     let shape = [| (size /. 2., height *. size); (size, 0.); (0., 0.) |] in
     Array.map (add_points position) shape
 ;;
+
 Graphics.open_graph " 1280*720-0+0";;
 divide 4 (getAcuteTriangle 300. (100., 100.)) Acute;;
 Unix.sleep 2;;
