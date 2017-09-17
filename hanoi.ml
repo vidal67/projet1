fc5a116f765c4ff185fc2aaf672afc854f3c6f37
Vidal Attias <vidalattias@MacBook-Pro-de-Vidal.local>
2017-09-17 09:56:15 +0200

    Premmiere version tours de Hanoi, marche pas du tout

--- hanoi.ml
+++ hanoi.ml
@@ -1,6 +1,24 @@
 (* The picks are represented as integer, 0 would be the leftmost pick
 and 1 the fist pick that is at the right of the leftmost pick, etc... *)
-type picks == int;;
+type picks = int;;
+
+module Stack =
+struct
+	type 'a t = {mutable elements : 'a list }
+	let create () = { elements = [] }
+	let push x s = s.elements <- x :: s.elements
+	let pop s =
+		match s.elements with
+		h::t -> s.elements <- t; h
+		| [] -> failwith "Empty stack"
+end;;
+
+let arrays = [|Stack.create ();Stack.create ();Stack.create ()|];;
+
+for i = 5 downto 1 do  Stack.push i arrays.(0); done;;
+
+arrays;;
+
 
 let moves_number = ref 0;;
 let increment_moves_number () = moves_number := !moves_number + 1;;
@@ -14,5 +32,15 @@ let moveDisc origin destination =
         print_string " to ";
         print_int destination;
         print_newline ()
-    end
-;;
+    end;;
+
+let arrays = Stack.create
+
+let rec hanoi n i j k =
+	if n = 1 then Stack.push (Stack.pop arrays.(i)) arrays.(j);;
+	else
+		hanoi (n-1) i k j;
+		hanoi 1 i j k;
+		hanoi (n-1) k j i
+		moveDisc i j;
+	done;;
